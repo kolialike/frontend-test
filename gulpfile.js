cbac34316fc3d17afd2ab67d977494ac258a5023
kolialike <kolialike@gmail.com>
2016-05-16 14:19:01 +0300

    first-commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,55 @@
-'use strict';
-
-var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    uglify = require('gulp-uglify'),
-    compass = require('gulp-compass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    rigger = require('gulp-rigger'),
-    cssmin = require('gulp-minify-css'),
-    imagemin = require('gulp-imagemin'),
-    rimraf = require('rimraf'),
-    plumber = require('gulp-plumber'),
-    connect = require('gulp-connect');
-
-
 var path = {
-    build: { //Тут мы укажем куда складывать готовые после сборки файлы
-        html: 'build/html',
+    src: {
+        dir: 'src/',
+        fonts: 'src/fonts/**/*.{eot,ttf,woff,woff2,svg}',
+        js: 'src/js/main.js',
+        sass: 'src/sass/*.{sass,scss}',
+        images: 'src/img/**/*.{jpg,png,gif,svg}',
+        html: 'src/html/*.html',
+        vendor: {
+            js: 'src/vendor/vendor.js',
+            css: 'src/vendor/*.scss'
+        }
+    },
+
+    watch: {
+        vendor: 'src/vendor/**/*.*',
+        fonts: 'src/fonts/**/*.{eot,ttf,woff,woff2,svg}',
+        js: 'src/js/**/*.js',
+        sass: 'src/sass/**/*.{sass,scss}',
+        html: 'src/html/**/*.html',
+        images: 'src/img/**/*.{jpg,png,gif,svg}'
+    },
+
+    build: {
+        dir: 'build/',
+        fonts: 'build/fonts/',
         js: 'build/js/',
         css: 'build/css/',
-        img: 'build/img/',
-        fonts: 'build/fonts/',
-        vendor_js: 'build/vendor/'
-    },
-    src: { //Пути откуда брать исходники
-        html: 'src/html/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
-        js: 'src/js/*.js',//В стилях и скриптах нам понадобятся только main файлы
-        sass: 'src/sass/*.sass',
-        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
-        fonts: 'src/fonts/**/*.*',
-        vendor_js: 'src/vendor/vendor.js'
-    },
-    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
-        html: 'src/html/**/*.html',
-        js: 'src/js/**/*.js',
-        sass: 'src/sass/**/*.sass',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*',
-        vendor_js: 'src/vendor/*.*'
-    },
-    clean: './build'
+        html: 'build/html',
+        images: 'build/img/'
+    }
 };
 
-//gulp webserver
-gulp.task('webserver', function () {
+var gulp = require('gulp');
+
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var debug = require('gulp-debug');
+var imagemin = require('gulp-imagemin');
+var plumber = require('gulp-plumber');
+var compass = require('gulp-compass');
+var rigger = require('gulp-rigger');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+var runSequence = require('run-sequence');
+var colors = require('colors');
+var cssmin = require('gulp-minify-css');
+
+var connect = require('gulp-connect');
+
+
+gulp.task('connect', function () {
     connect.server({
         root: '../',
         port: '8080',
@@ -50,131 +57,163 @@ gulp.task('webserver', function () {
     });
 });
 
-//html build
+
+gulp.task('clean', function () {
+    return gulp.src(path.build.dir)
+        .pipe(clean({force: true}));
+});
+
 gulp.task('html:build', function () {
-    gulp.src(path.src.html) //Выберем файлы по нужному пути
+    gulp.src(path.src.html)
         .pipe(plumber({
             errorHandler: function (error) {
                 console.log(error.message);
                 this.emit('end');
             }
         }))
-        .pipe(rigger()) //Прогоним через rigger
-        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
-        .pipe(connect.reload()); //И перезагрузим наш сервер для обновлений
+        .pipe(debug({title: 'html:'}))
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
+        .pipe(connect.reload());
+    ;
 });
 
-//Js build
+gulp.task('fonts:build', function () {
+    return gulp.src(path.src.fonts)
+        .pipe(debug({title: 'fonts:'}))
+        .pipe(gulp.dest(path.build.fonts))
+        .pipe(connect.reload());
+    ;
+});
+
+gulp.task('images:build', function () {
+    return gulp.src(path.src.images)
+        .pipe(debug({title: 'images:'}))
+        .pipe(imagemin({
+            progressive: true,
+            interlaced: true,
+            svgoPlugins: [{removeViewBox: false}]
+        }))
+        .pipe(gulp.dest(path.build.images))
+        .pipe(connect.reload());
+    ;
+});
+
+gulp.task('vendor:js:build', function () {
+    return gulp.src(path.src.vendor.js)
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(debug({title: 'vendor js:'}))
+        .pipe(rigger())
+        .pipe(uglify())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(connect.reload());
+    ;
+});
+
+gulp.task('vendor:css:build', function () {
+    return gulp.src(path.src.vendor.css)
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(debug({title: 'vendor css:'}))
+        .pipe(rigger())
+        .pipe(cssmin())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(connect.reload());
+    ;
+});
+
 gulp.task('js:build', function () {
-    gulp.src(path.src.js) //Найдем наш main файл
+    return gulp.src(path.src.js)
         .pipe(plumber({
             errorHandler: function (error) {
                 console.log(error.message);
                 this.emit('end');
             }
         }))
-        .pipe(rigger()) //Прогоним через rigger
-        .pipe(sourcemaps.init()) //Инициализируем sourcemap
-        .pipe(uglify()) //Сожмем наш js
-        .pipe(sourcemaps.write()) //Пропишем карты
-        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
-        .pipe(connect.reload()); //И перезагрузим сервер
+        .pipe(debug({title: 'js:'}))
+        .pipe(rigger())
+        .pipe(uglify())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(connect.reload());
+    ;
 });
 
-//sass build
 gulp.task('sass:build', function () {
-    gulp.src(path.src.sass) //Выберем наш main.scss
+    return gulp.src(path.src.sass)
         .pipe(plumber({
             errorHandler: function (error) {
                 console.log(error.message);
                 this.emit('end');
             }
         }))
-        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(debug({title: 'sass:'}))
         .pipe(compass({
             sass: 'src/sass',
             css: path.build.css,
-            image: path.build.img,
-            font: path.build.fonts,
+            image: 'src/img',
+            font: 'src/fonts'
         }))
-        .pipe(cssmin()) //Сожмем
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(cssmin())
+        .pipe(gulp.dest(path.build.css))
         .pipe(connect.reload());
 });
 
-//img build
-gulp.task('image:build', function () {
-    gulp.src(path.src.img) //Выберем наши картинки
-        .pipe(imagemin({ //Сожмем их
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            interlaced: true
-        }))
-        .pipe(gulp.dest(path.build.img)) //И бросим в build
-        .pipe(connect.reload());
+gulp.task('vendor:build', function () {
+    runSequence('vendor:js:build', 'vendor:css:build');
 });
 
-//fonts build
-gulp.task('fonts:build', function() {
-    gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
+gulp.task('rebuild', function () {
+    runSequence(
+        'clean',
+        'html:build',
+        'fonts:build',
+        'images:build',
+        ['vendor:js:build', 'vendor:css:build'],
+        'js:build',
+        'sass:build',
+        'images:build',
+        function () {
+            console.log('project is ' + colors.green.underline('rebuild'));
+        }
+    );
 });
 
-//task vendor js
-gulp.task('vendor_js:build', function () {
-    gulp.src(path.src.vendor_js) //Найдем наш main файл
-        .pipe(plumber({
-            errorHandler: function (error) {
-                console.log(error.message);
-                this.emit('end');
-            }
-        }))
-        .pipe(rigger()) //Прогоним через rigger
-        .pipe(uglify()) //Сожмем наш js
-        .pipe(gulp.dest(path.build.vendor_js)) //Выплюнем готовый файл в build
-        .pipe(connect.reload()); //И перезагрузим сервер
-});
+gulp.task('watch', function () {
 
-//gulp clean
-gulp.task('clean', function (cb) {
-    rimraf(path.clean, cb);
-});
-
-//gulp build
-gulp.task('build', [
-    'clean',
-    'html:build',
-    'js:build',
-    'sass:build',
-    'fonts:build',
-    'image:build',
-    'vendor_js:build'
-]);
-
-//gulp watch
-gulp.task('watch', function(){
-    watch([path.watch.html], function(event, cb) {
+    watch([path.watch.html], function () {
         gulp.start('html:build');
     });
-    watch([path.watch.sass], function(event, cb) {
-        gulp.start('sass:build');
-    });
-    watch([path.watch.js], function(event, cb) {
-        gulp.start('js:build');
-    });
-    watch([path.watch.img], function(event, cb) {
-        gulp.start('image:build');
-    });
-    watch([path.watch.fonts], function(event, cb) {
+
+    // watch([path.watch.images], function () {
+    //     gulp.start('images:build');
+    // });
+
+    watch([path.watch.fonts], function () {
         gulp.start('fonts:build');
     });
-    watch([path.watch.vendor_js], function(event, cb) {
-        gulp.start('vendor_js:build');
+
+    watch([path.watch.vendor], function () {
+        gulp.start('vendor:build');
     });
+
+    watch([path.watch.js], function () {
+        gulp.start('js:build');
+    });
+
+    watch([path.watch.sass], function () {
+        gulp.start('sass:build');
+    });
+
 });
 
 
-
-//gulp default
-gulp.task('default', [ 'build', 'webserver', 'watch']);
+gulp.task('default', ['connect', 'rebuild', 'watch']);
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,47 @@
 jQuery(function($){
-  $('.flexslider').flexslider({
-    animation: "slide"
+   var body = $("body");
+  //menu
+  var menuBurger = $(".menu-burger");
+  menuBurger.on('click', function(event) {
+  	event.preventDefault();
+  	body.toggleClass('mobile-menu-open');
   });
+  var mobileMenu = $(".mobile-menu");
+  mobileMenu.on('click', function(event) {
+  	event.preventDefault();
+  	body.removeClass("mobile-menu-open");
+  });
+  var mobileMenuItem = $(".mobile-menu-item");
+  mobileMenuItem.on('click', function(event) {
+  	event.stopPropagation();
+  });
+
+  // menu-list-tile
+  var menuTileList = $(".menu-tile-list a");
+  var menuTile = $("#menu-tile");
+  var menuList = $("#menu-list");
+  menuTileList.on('click', function(event) {
+  	event.preventDefault();
+  	$(this).addClass('active').siblings().removeClass('active');
+  	var index = menuTileList.index($(this));
+  	console.log(index);
+  	if (index == 0) {
+  		menuTile.addClass('open');
+  		menuList.removeClass('open');
+  	} else {
+		menuTile.removeClass('open');
+  		menuList.addClass('open');
+  	}
+  });
+
+  //menu-navigation
+  var menuNavigation = $(".menu-navigation");
+	menuNavigation.on('click', function(event) {
+		event.preventDefault();
+		menuNavigation.toggleClass('menu-navigation-open');
+	});
+	menuNavigation.on('click', 'a', function(event) {
+		event.preventDefault();
+		$(this).addClass("active").parent('li').siblings('li').children('a').removeClass('active');
+	});
 });
